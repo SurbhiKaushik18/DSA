3d965fe6be26ab46a7335aaaa0b599a3f2183c36
SurbhiKaushik18 <sliet.surbhisingh2000@gmail.com>
2024-08-23 07:48:19 +0530

    DSA Practise

---
+++ dsa4.py
@@ -0,0 +1,8 @@
+a=input("Enter the value: ")
+if a.isupper():
+    print("Uppercase")
+elif a.islower():
+    print("lowercase")
+else:
+    print("Numeric")
+
---
+++ dsa5.py
@@ -0,0 +1,5 @@
+a=int(input("Enter the number:"))
+i=0
+while i<a:
+    i+=1
+    print(i)
